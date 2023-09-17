81bfbc22235d868146c467579cde61a09ebb007a
Guilherme Castelo <guilhermecastelo.mail@gmail.com>
2023-09-17 01:13:46 -0400

    init project

---
+++ src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { InstrumentsModule } from './instruments/instruments.module';
+import { CommonModule } from './common/common.module';
+import { RoleModule } from './role/role.module';
+import { UserModule } from './user/user.module';
+import { ShiftModule } from './shift/shift.module';
+import { ClassroomModule } from './classroom/classroom.module';
+import { ImagesModule } from './images/images.module';
+import { EventsModule } from './events/events.module';
+
+@Module({
+  imports: [InstrumentsModule, CommonModule, RoleModule, UserModule, ShiftModule, ClassroomModule, ImagesModule, EventsModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
